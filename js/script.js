851ed475feb04a6ccdf9ae6e827b3acba3c60de4
Damian Godek <damian.godek1@gmail.com>
2018-10-28 00:36:14 +0200

    add counter

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var Counter = React.createClass({
+
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {onClick: this.increment},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        );
+    }
+});
+
+var Buttons =
+  React.createElement('div', {},
+    React.createElement('button', {}, 'plus'),
+    React.createElement('button', {}, 'minus')
+    );  
+
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
