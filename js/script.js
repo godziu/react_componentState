bbd7e467a8d6ffc71a88dd0d316b023ddcd4360f
Damian Godek <damian.godek1@gmail.com>
2018-10-28 10:44:48 +0100

    add console log with component state

--- js/script.js
+++ js/script.js
@@ -18,18 +18,49 @@ var Counter = React.createClass({
         });
     },
 
+    componentWillMount: function() {
+        console.log('wykonuje się przed render');
+    },
+
+    componentDidMount: function() {
+        console.log('wyświetla komponenty na stronie');
+    },
+
+    componentWillReceiveProps: function() {
+        console.log('wykonuje się po dodaniu nowych propsów');
+    },
+
+    shouldComponentUpdate: function() {
+        console.log('sprawdza czy przerysowuje komponent');
+        return true;
+    },
+
+    componentWillUpdate: function() {
+        console.log('przygotowuje uaktualnienia');
+    },
+
+    componentDidUpdate: function() {
+        console.log('wyświetla komponent po uaktualnieniu');
+    },
+
+    componentWillUnmount: function() {
+        console.log('usuwa niepotrzebne komponenty');
+    },
+
     render: function() {
-        return React.createElement('div', {onClick: this.increment},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        return (
+            React.createElement('div', {},
+                React.createElement('div', {className: 'counter'},
+                    React.createElement('button',{onClick: this.increment}, '+'),
+                    React.createElement('div', {}, 'Counter:' + this.state.counter),
+                    React.createElement('button', {onClick: this.decrement}, '-'),
+                    )
+                )
+            
         );
     }
 });
 
-var Buttons =
-  React.createElement('div', {},
-    React.createElement('button', {}, 'plus'),
-    React.createElement('button', {}, 'minus')
-    );  
 
 
 var element = React.createElement(Counter);
